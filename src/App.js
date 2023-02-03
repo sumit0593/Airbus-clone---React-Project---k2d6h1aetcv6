33c14f5c9917ac6cb37aa0d922e48b5b5e54ffce
sumit0593 <sumit0593>
2023-02-03 17:57:22 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Navbar from './components/Navbar';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
-import Result from './components/result'
-import { MediumProvider } from './context/MediumContext'
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Result from "./components/result";
+import { MediumProvider } from "./context/MediumContext";
 
 function App() {
   return (
-      <div>
+    <div>
       <MediumProvider>
-      <Router>
-      <Navbar/>
-        <Routes>
-          <Route exact path='/' element={<Dashboard/>} >
-          </Route>
-          <Route exact path='/:id' element={<Dashboard/>} >
-          </Route>
-          <Route exact path='/checkout' element={<Result/>} >
-          </Route>
-        </Routes>
-      </Router>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />}></Route>
+            <Route exact path="/:id" element={<Dashboard />}></Route>
+            <Route exact path="/checkout" element={<Result />}></Route>
+          </Routes>
+        </Router>
       </MediumProvider>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { GoogleAuthProvider } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,14 +14,14 @@ const firebaseConfig = {
   storageBucket: "airbus-clone-react.appspot.com",
   messagingSenderId: "610223257861",
   appId: "1:610223257861:web:dd244e151e20d711acdf6c",
-  measurementId: "G-3C102WFWBG"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  measurementId: "G-3C102WFWBG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-export { auth , provider , db };
+export { auth, provider, db };
